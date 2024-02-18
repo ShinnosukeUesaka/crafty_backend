e47f4dbded545d88a1c78f78fe92663f1ea73a30
ShinnosukeUesaka <shinnosuke.uesaka@gmail.com>
2024-02-17 20:55:51 -0800

    Add audio transcription endpoint

--- crafty/main.py
+++ crafty/main.py
@@ -5,6 +5,9 @@ dotenv.load_dotenv('crafty/.env', override=True)
 from pathlib import Path
 from fastapi import FastAPI, Header, HTTPException, Request
 from fastapi.middleware.cors import CORSMiddleware
+from fastapi.responses import JSONResponse
+# for audio transcription input, file
+from fastapi import UploadFile, File
 from pydantic import BaseModel
 import json
 from openai import OpenAI
@@ -27,12 +30,31 @@ app.add_middleware(
 class ExampleInput(BaseModel):
     text: str
 
+# audio transcription endpoint
+@app.post("/transcribe")
+def transcribe_audio(file: UploadFile = File(...)):
+    print(file)
+    # audio_file= open("/path/to/file/audio.mp3", "rb")
+    # transcript = client.audio.transcriptions.create(
+    #     model="whisper-1", 
+    #     file=audio_file
+    # )
+    # use the file to transcribe
     
+    audio_file = file.file.read()
+    transcript = client.audio.transcriptions.create(
+        model="whisper-1", 
+         file=audio_file
+    )
+    transcribed_text = transcript.text
+    
+    return {"transcribed_text": transcribed_text}
 
 # crafty endpoint
 @app.get("/")
 def read_root():
     return {"Hello": "World"}
+
 @app.post("/generate")
 def generate(input: ExampleInput):
     response = client.chat.completions.create(
